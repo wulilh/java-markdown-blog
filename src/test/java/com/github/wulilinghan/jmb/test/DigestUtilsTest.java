d17723cdcad7e629a8a1e1926474ed006cf51bee
wulilinghan <donlinghan@gmail.com>
2023-02-01 00:20:32 +0800

    test: DigestUtilsTest

--- src/test/java/com/github/wulilinghan/jmb/test/DigestUtilsTest.java
+++ src/test/java/com/github/wulilinghan/jmb/test/DigestUtilsTest.java
@@ -1,12 +1,16 @@
 package com.github.wulilinghan.jmb.test;
 
 import com.github.wulilinghan.jmb.TestBase;
+import com.github.wulilinghan.jmb.common.utils.HashCode;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author wuliling Created By 2023-01-17 10:20
@@ -14,8 +18,29 @@ import java.io.IOException;
 public class DigestUtilsTest extends TestBase {
 
     @Test
+    public void test_md5HexToNum2() throws NoSuchAlgorithmException {
+        final String lowerCase = "d:\\website\\md\\Docker.md".toLowerCase();
+
+        final MessageDigest md = MessageDigest.getInstance("MD5");
+        final byte[] digest = md.digest(lowerCase.getBytes());
+        final BigInteger bigInteger = new BigInteger(1, digest);
+        final String uniId = bigInteger.toString(16);
+        System.out.println("uniId = " + uniId);
+    }
+
+    @Test
+    public void test_md5HexToNum() {
+        final String lowerCase = "d:\\website\\md\\Docker.md".toLowerCase();
+        String md5Hex = DigestUtils.md5Hex(lowerCase);
+        System.out.println("md5Hex = " + md5Hex); //0fc52e05dd58aedb41df012b99272299
+        final String md5ToNum = HashCode.md5ToUniqueNum(md5Hex);
+        System.out.println("md5ToNum = " + md5ToNum);//0650304960627345
+    }
+
+    @Test
     public void test_md5Hex() {
-        String sha256Hex = DigestUtils.md5Hex("ABCDEFG");
+        final String lowerCase = "d:\\website\\md\\Docker.md".toLowerCase();
+        String sha256Hex = DigestUtils.md5Hex(lowerCase);
         System.out.println("sha256Hex = " + sha256Hex);
     }
 
